860dd4dded11f29907c45fd496add2469c921665
developpeuse28 <n3ptune28@gmail.com>
2023-07-08 16:00:19 +0900

    first commit

---
+++ src/main/java/com/example/doyoulike/DoYouLikeApplication.java
@@ -0,0 +1,13 @@
+package com.example.doyoulike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoYouLikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoYouLikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/doyoulike/DoYouLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doyoulike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoYouLikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
